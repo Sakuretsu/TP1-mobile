a69bf50a8ef3b6d9140b3f8a5c8089f9cee8bc37
Sakuretsu <korvax1@gmail.com>
2017-01-16 14:00:22 -0500

    Model

---
+++ app/src/main/java/com/v41/tp1/ColorGame.java
@@ -0,0 +1,39 @@
+package com.v41.tp1;
+
+import android.graphics.Color;
+import android.support.annotation.ColorRes;
+import android.support.annotation.StringRes;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Tommy Bouffard on 2017-01-16.
+ */
+
+public class ColorGame {
+
+    private int colorToFind;
+    private int[] colorDisplayNames;
+
+    private ColorItem[] colorItems;
+
+    public ColorGame(ColorItem[] colorItems){
+        this.colorItems = colorItems;
+        colorDisplayNames = new int[4];
+    }
+
+    public void StartNewGame(){
+        Collections.shuffle(Arrays.asList(colorItems));
+        for(int i = 0; i<colorDisplayNames.length; i++){
+             colorDisplayNames[i] = colorItems[i].name;
+        }
+        Random rn = new Random();
+        colorToFind = colorDisplayNames[rn.nextInt(colorDisplayNames.length)];
+    }
+
+    public boolean CompareSelection(int pushedButtonColorCode){
+        return colorToFind == pushedButtonColorCode;
+    }
+}
---
+++ app/src/main/java/com/v41/tp1/ColorItem.java
@@ -0,0 +1,15 @@
+package com.v41.tp1;
+
+import android.support.annotation.ColorRes;
+import android.support.annotation.StringRes;
+
+/**
+ * Created by Tommy Bouffard on 2017-01-16.
+ */
+
+public class ColorItem {
+    @StringRes
+    public int name;
+    @ColorRes
+    public int color;
+}
