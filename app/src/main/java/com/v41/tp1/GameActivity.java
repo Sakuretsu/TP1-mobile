f79ca2f20f5f14d0bbd382360f1c5eefbe049be9
Sakuretsu <korvax1@gmail.com>
2017-01-20 07:56:02 -0500

    Fix crash orientation change

--- app/src/main/java/com/v41/tp1/GameActivity.java
+++ app/src/main/java/com/v41/tp1/GameActivity.java
@@ -80,5 +80,15 @@ public class GameActivity extends AppCompatActivity {
         game.startNewGame();
         UpdateButtonTextAndColor();
         }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
 
